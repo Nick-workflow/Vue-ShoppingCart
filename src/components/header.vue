08ada16b100a1dff01c0fb06bc4d4803c36e5812
yangtao <yangtaoeng@gmail.com>
2017-05-31 23:51:12 +0800

    Vue-ShoppingCart

---
+++ src/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="container-fluid">
+		<div class="navbar navbar-inverse">
+			<router-link  to="/" class="navbar-brand">商店</router-link>
+			<div class="navbar-right">
+				<div class="navbar-text">
+					<b>你的购物车</b>
+					{{sum}} 件,￥{{total}}.00
+				</div>
+				<router-link to="/checkout" class="btn btn-default navbar-btn">checkout</router-link>
+			</div>
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+export default{
+	computed:{
+		total(){
+			return this.$store.getters.total
+		},
+		sum(){
+			return this.$store.getters.sum
+		}
+	}
+}
+</script>
+<style>
+.navbar-right{
+	float: right !important;
+	margin-right: 5px;
+}
+.navbar-text{
+	margin-right: 10px;
+}	
+</style>
